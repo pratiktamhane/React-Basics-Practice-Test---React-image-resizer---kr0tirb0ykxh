8a3855602f89fc6276b21cb3f291153bad35960d
pratiktamhane <96224999+pratiktamhane@users.noreply.github.com>
2022-04-01 11:35:34 +0530

    p

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
-import React from 'react'
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
+import Image from "./src/image.jpeg";
 
 const App = () => {
+  const [imageToResize, setImageToResize] = useState(undefined);
+
+  const onHeight = () => {
+    setImageToResize(imageToResize);
+  };
   return (
     <div id="main">
-
+      <h2>Resized Image</h2>
+      <img id="resizable-img" width="320" src={Image} />
+      <div onChange={onHeight}>
+        <input type="range" min={100} max={800} />
+        <input type="range" min={100} max={800} />
+      </div>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
